d8053f43db09dd89db7e54ff2a07791cced8cc21
Christian <cosechasistema@gmail.com>
2020-04-18 03:34:11 +0000

    primer comit

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,19 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		// return view('welcome_message');
+		echo "Esta es la funcion Index";
+	}
+public function test() {
+  echo "esto es una prueba test";
+}
+
+public function test2() {
+  echo "esto es una prueba test2";
+}
+	//--------------------------------------------------------------------
+
+}
